30ca9b864f84bd52195d8c65245fcf16ac37adfb
hankhester <hankhester1@gmail.com>
2016-06-15 14:00:09 -0400

    took away background color

--- Peru/src/application/Main.java
+++ Peru/src/application/Main.java
@@ -7,16 +7,13 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 
 public class Main extends Application {	
-	
-	Color tarheelBlue = new Color(0.482, 0.686, 0.831, 1);
-	
+		
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,600,600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			scene.setFill(tarheelBlue);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 			System.out.println("this is a new message");
