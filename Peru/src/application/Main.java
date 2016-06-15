9c57262dc74d04e262fbfe87db739243b42b6dda
hankhester <hankhester1@gmail.com>
2016-06-15 13:53:57 -0400

    added "the app ran" console message

--- Peru/src/application/Main.java
+++ Peru/src/application/Main.java
@@ -19,6 +19,7 @@ public class Main extends Application {
 			scene.setFill(tarheelBlue);
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			System.out.println("the app ran!");
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
