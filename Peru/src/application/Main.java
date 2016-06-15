717ddcb9121931b2ae85f88169596acbca7a1e31
hankhester <hankhester1@gmail.com>
2016-06-15 14:11:34 -0400

    changed color to carolina blue

--- Peru/src/application/Main.java
+++ Peru/src/application/Main.java
@@ -7,6 +7,8 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 
 public class Main extends Application {	
+	
+	Color tarheelBlue = new Color(0.482, 0.686, 0.831, 1);
 			
 	@Override
 	public void start(Stage primaryStage) {
@@ -15,7 +17,7 @@ public class Main extends Application {
 			Scene scene = new Scene(root,600,600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
-			scene.setFill(Color.BLUE);
+			scene.setFill(tarheelBlue);
 			primaryStage.show();
 			System.out.println("this is a new message");
 		} catch(Exception e) {
