0a8ccf610a3fcd137acc4cae75541dce755fe6f1
hankhester <hankhester1@gmail.com>
2016-06-16 15:44:29 -0400

    added instance variables and constructor to Word class

--- Peru/src/applicationOriginal/Word.java
+++ Peru/src/applicationOriginal/Word.java
@@ -1,5 +1,26 @@
 package applicationOriginal;
 
+import java.net.URL;
+
+import javafx.scene.image.Image;
+import javafx.scene.media.Media;
+
 public class Word {
+	
+	private String _english;
+	private String _quechua;
+	private Image _image;
+	private Media _sound;
+	
+	Word(String english, String quechua, Image image, String filename) {
+		_english = english;
+		_quechua = quechua;
+		_image = image;
+	    URL resource = this.getClass().getResource("sounds/" + filename);
+	    Media media = new Media(resource.toString());
+	    _sound = media;
+	}
+	
+	
 
 }
